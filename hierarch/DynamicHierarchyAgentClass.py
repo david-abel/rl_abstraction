2099f264475134c1989327d7050e7829a7918ff5
david-abel <david_abel@brown.edu>
2017-10-12 13:01:25 -0400

    adding new controller

--- hierarch/DynamicHierarchyAgentClass.py
+++ hierarch/DynamicHierarchyAgentClass.py
@@ -15,25 +15,8 @@ class DynamicHierarchyAgent(HierarchyAgent):
         Return:
             (str)
         '''
-        # self.prev_state = None
-        # self.prev_action = None
-
         new_level = self._compute_max_v_hat_level(ground_state)
         self.set_level(new_level)
-
-            # if self.sa_stack.phi(self.prev_state, self.cur_level) != self.sa_stack.phi(ground_state, self.cur_level):
-            #     # We've changed clusters. Go up.
-            #     self.incr_level()
-            # else:
-            #     # Same cluster, track experience.
-            #     abstr_state = self.sa_stack.phi(ground_state, self.cur_level)
-            #     if (self.prev_state, ground_state) in self.seen_transitions[abstr_state]:
-            #         # If we've seen it...
-            #         self.known[abstr_state] = True
-            #         self.incr_level()
-            #     else:
-            #         self.decr_level()
-
         action = HierarchyAgent.act(self, ground_state, reward)
 
         return action
