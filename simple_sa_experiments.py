2609d9de9c5ca9da55d60c85a4232c64aee9a6f0
david-abel <david_abel@brown.edu>
2017-10-24 09:53:00 -0400

    ADded a script for running simple state abstr experiments

---
+++ simple_sa_experiments.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# Python imports.
+import random
+from collections import defaultdict
+import os
+
+# Other imports.
+from simple_rl.utils import make_mdp
+from simple_rl.agents import RandomAgent, RMaxAgent, QLearnerAgent, FixedPolicyAgent
+from simple_rl.run_experiments import run_agents_multi_task
+from state_abs.StateAbstractionClass import StateAbstraction
+from action_abs.ActionAbstractionClass import ActionAbstraction
+from AbstractionWrapperClass import AbstractionWrapper
+from state_abs import indicator_funcs as ind_funcs
+from abstraction_experiments import get_sa
+
+def main():
+
+    # Grab experiment params.
+    mdp_class = "four_room"
+    task_samples = 100
+    episodes = 100
+    steps = 200
+    grid_dim = 15
+    gamma = 0.95
+
+    # ========================
+    # === Make Environment ===
+    # ========================
+    environment = make_mdp.make_mdp_distr(mdp_class=mdp_class, grid_dim=grid_dim)
+    actions = environment.get_actions()
+    environment.set_gamma(gamma)
+    sa_to_test = get_sa(environment, indic_func=ind_funcs._q_disc_approx_indicator, epsilon=0.00)
+
+    # ===================
+    # === Make Agents ===
+    # ===================
+    rand_agent = RandomAgent(actions)
+    baseline_agent = QLearnerAgent(actions, gamma=gamma, epsilon=0.1, alpha=0.05)
+    sa_agent = AbstractionWrapper(QLearnerAgent, actions, str(environment), state_abstr=sa_to_test, name_ext="sa")
+    agents = [baseline_agent, rand_agent, sa_agent]
+
+    # Run!
+    run_agents_multi_task(agents, environment, task_samples=task_samples, steps=steps, episodes=episodes, reset_at_terminal=True)
+
+
+if __name__ == "__main__":
+	main()
